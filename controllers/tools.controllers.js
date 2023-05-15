9dd18a247ed0264be1645ad40a65e3e389db795d
AKM SYFUL ALOM <84580139+akmsyfulalom@users.noreply.github.com>
2023-05-16 00:47:55 +0600

    error handler add now

--- controllers/tools.controllers.js
+++ controllers/tools.controllers.js
@@ -3,8 +3,6 @@ let tools = [
     {id: 2, name: 'hmm2'},
     {id: 3, name: 'hmm3'}
 ];
-
-
 module.exports.getAllTools = (req, res, next) =>{
     // const {ip, query, params, body, headers} = req;
     // console.log(ip, query, params, body, headers);
@@ -14,23 +12,19 @@ module.exports.getAllTools = (req, res, next) =>{
     // res.send("ok")
     const {limit, page} = req.query;
     console.log(limit, page);
-    res.json(tools.slice(0, limit))
+    res.status(200).json(tools.slice(0, limit))
 };
-
 module.exports.saveATool = (req, res, next) =>{
     console.log(req.query)
     tools.push(req.body)
-    res.send(tools)
-    
+    res.status(200).send(tools) 
 };
-
 module.exports.getToolDetail = (req, res) =>{
 const {id} = req.params;
 console.log(id);
 const foundTool = tools.find(tool => tool.id === Number(id));
-res.send(foundTool);
+res.status(200).send(foundTool);
 };
-
 module.exports.updateTool = (req, res) =>{
     // const newData = req.body;
     const {id} = req.params;
@@ -38,22 +32,15 @@ module.exports.updateTool = (req, res) =>{
     const newData = tools.find(tool => tool.id ===Number(id));
     newData.id = id;
     newData.name = req.body.name;
-    res.send(newData);
+    res.status(200).send(newData);
 };
-
 module.exports.deleteTool = (req, res) =>{
     const { id } = req.params;
     const filter = { _id: id };
-
    tools = tools.filter(tool => tool.id !== Number(id))
-
-   res.send(tools)
-
-
-
+   res.status(200).Number
+send(tools)
 };
-
-
 // module.exports = {
 //     getAllTools
 // }
