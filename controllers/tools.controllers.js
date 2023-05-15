076c36c37221792d899f51d743c133a4e0a575e2
AKM SYFUL ALOM <84580139+akmsyfulalom@users.noreply.github.com>
2023-05-16 01:35:39 +0600

    update for res

--- controllers/tools.controllers.js
+++ controllers/tools.controllers.js
@@ -17,13 +17,29 @@ module.exports.getAllTools = (req, res, next) =>{
 module.exports.saveATool = (req, res, next) =>{
     console.log(req.query)
     tools.push(req.body)
-    res.status(200).send(tools) 
+    res.status(200).send({
+        success: true,
+        message: "Success",
+        data: tools
+    }) 
+    res.status(500).send({
+        success: false,
+        error: "Internal Server Error"
+    })
 };
 module.exports.getToolDetail = (req, res) =>{
 const {id} = req.params;
 console.log(id);
 const foundTool = tools.find(tool => tool.id === Number(id));
-res.status(200).send(foundTool);
+res.status(200).send({
+    success: true,
+    message: "Success",
+    data: foundTool
+});
+res.status(500).send({
+    success: false,
+    error: "Internal Server Error"
+})
 };
 module.exports.updateTool = (req, res) =>{
     // const newData = req.body;
@@ -32,14 +48,30 @@ module.exports.updateTool = (req, res) =>{
     const newData = tools.find(tool => tool.id ===Number(id));
     newData.id = id;
     newData.name = req.body.name;
-    res.status(200).send(newData);
+    res.status(200).send({
+        success: true,
+        message: "Success",
+        data: newData
+    });
+    res.status(500).send({
+        success: false,
+        error: "Internal Server Error"
+    });
 };
 module.exports.deleteTool = (req, res) =>{
     const { id } = req.params;
     const filter = { _id: id };
    tools = tools.filter(tool => tool.id !== Number(id))
    res.status(200).Number
-send(tools)
+send({
+    success: true,
+    message: "Success",
+    data: tools
+})
+res.status(500).send({
+    success: false,
+    error: "Internal Server Error"
+})
 };
 // module.exports = {
 //     getAllTools
